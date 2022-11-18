78ec8a6903117414edbbc01e1f1d02fd62628d40
Alex Oleynik <alexoleynik74@gmail.com>
2022-11-18 16:05:10 -0600

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
        <>
        <div className="footer">
-        <IconContext.Provider value={{ color: 'red', size: '50px'}}>
+        <IconContext.Provider value={{ color: 'tan', size: '50px'}}>
         <a href="https://github.com/AlexO16" target='_blank' rel="noreferrer"><BsGithub /></a>
         <a href="https://www.linkedin.com/" target='_blank' rel="noreferrer"><BsLinkedin /></a>
         <a href="https://stackoverflow.com/" target='_blank' rel="noreferrer"><BsStackOverflow /></a>
