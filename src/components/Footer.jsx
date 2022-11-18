ebc6ead85705d754fc46e8e357e63bb41281a894
Alex Oleynik <alexoleynik74@gmail.com>
2022-11-18 15:55:10 -0600

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import { BsGithub, BsLinkedin, BsStackOverflow } from 'react-icons/bs';
+import { IconContext } from 'react-icons/lib';
 
 
 const Footer = () => {
     return (
        <>
        <div className="footer">
+        <IconContext.Provider value={{ color: 'red', size: '50px'}}>
         <a href="https://github.com/AlexO16" target='_blank' rel="noreferrer"><BsGithub /></a>
         <a href="https://www.linkedin.com/" target='_blank' rel="noreferrer"><BsLinkedin /></a>
         <a href="https://stackoverflow.com/" target='_blank' rel="noreferrer"><BsStackOverflow /></a>
+        </IconContext.Provider>
         </div>
+        
        </> 
     )
 }
